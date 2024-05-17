15766c541bbc16d8b329f969a1d87e8d3519899b
Almosk <alvin.kita@outlook.fr>
2024-05-17 12:51:04 +0200

    Ajout des HAL

--- commande/src/main/java/com/ipi/jva324/commande/service/CommandeProduitServiceLocalImpl.java
+++ commande/src/main/java/com/ipi/jva324/commande/service/CommandeProduitServiceLocalImpl.java
@@ -3,9 +3,9 @@ package com.ipi.jva324.commande.service;
 import com.ipi.jva324.stock.model.ProduitEnStock;
 import com.ipi.jva324.stock.service.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class CommandeProduitServiceLocalImpl implements CommandeProduitService {
 
     @Autowired
