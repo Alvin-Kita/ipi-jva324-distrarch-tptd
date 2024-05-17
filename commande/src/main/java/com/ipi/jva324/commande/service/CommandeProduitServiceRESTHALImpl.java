baa8cd2ad2a9132f89ede157b2952b7ee7a20791
Almosk <alvin.kita@outlook.fr>
2024-05-17 14:50:48 +0200

    some fix

--- commande/src/main/java/com/ipi/jva324/commande/service/CommandeProduitServiceRESTHALImpl.java
+++ commande/src/main/java/com/ipi/jva324/commande/service/CommandeProduitServiceRESTHALImpl.java
@@ -4,6 +4,7 @@ import com.ipi.jva324.stock.model.ProduitEnStock;
 import com.ipi.jva324.stock.service.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +15,12 @@ public class CommandeProduitServiceRESTHALImpl implements CommandeProduitService
     @Autowired @Qualifier("springDataRestTemplate")
     private RestTemplate restTemplate;
 
+    /** URL serveur : (configurée dans application.properties, sinon valeur par défaut) */
+    @Value("${monsuperserver.url:http://localhost:8080/}")
+    private String url;
+
     @Override
     public ProduitEnStock getProduit(long id) {
-        return restTemplate.getForObject( "http://localhost:8080/api/data-rest/produitEnStocks/" + id, ProduitEnStock.class);
+        return restTemplate.getForObject( url + "api/data-rest/produitEnStocks/" + id, ProduitEnStock.class);
     }
 }
